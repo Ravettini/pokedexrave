73f86bf15450caeda5534f0bf430eb98420b11cc
Ravettini <ignaciorave21@gmail.com>
2023-11-04 00:40:07 -0300

    errores reactrouter

--- src/AppRouter.jsx
+++ src/AppRouter.jsx
@@ -1,13 +1,15 @@
 import React from 'react'
 import { Routes, Route, Navigate } from 'react-router-dom'
 import { Navigation } from './components/Navigation'
-import { HomePage, PokemonPage, SearchPage } from './pages'
+import { HomePage, PokemonPage, SearchPage,  } from './pages'
+import Heropage from './pages/HeroPage'
 
 export const AppRouter = () => {
   return (
     <Routes> 
+            <Route index element={<Heropage/>}/>
         <Route path='/' element={<Navigation/>}>
-            <Route index element={<HomePage/>}/>
+            <Route path='/pages'  element={<HomePage/>}/>
             <Route path='pokemon/:id' element={<PokemonPage/>}/> 
             <Route path='search' element={<SearchPage/>}/>
         </Route>
--- src/pages/HeroPage.jsx
+++ src/pages/HeroPage.jsx
@@ -1,15 +1,64 @@
-import React from 'react'
-import pokebaner from '../assets/img/pokebanner.jpg'
+import React, { useState, useEffect, useRef } from 'react';
+import profesor from '../assets/img/Profesor_Oak_LGPE.png'
+import sound from '../assets/sounds/sonidito.mp3';
+import sound2 from '../assets/sounds/cancionOak.mp3';
+import silly from '../assets/img/silly.png'
 
-export const HeroPage = () => {
+const Heropage = () => {
+  const audioRef = useRef(null);
+1
+  const textos = [
+    'Hola, ¡bienvenido al mundo Pokémon!.',
+    'Mi nombre es Oak, pero la gente me llama profesor Pokémon.',
+    'Este mundo está habitado por criaturas llamadas Pokémon.',
+    'Algunos los usan de mascota, otros los usan para luchar,',
+    'mientras que yo los estudio como profesión.',
+    'Esto que te entregaré se llama Pokédex.',
+    'La cual es una base de datos de todos los pokemones del mundo',
+    '¡Échale un vistazo!'
+  ];
+  const [indiceTexto, setIndiceTexto] = useState(0);
+  const [textoMostrado, setTextoMostrado] = useState('');
+  const [caracterActual, setCaracterActual] = useState(0);
+
+  useEffect(() => {
+    if (indiceTexto < textos.length) {
+      if (caracterActual < textos[indiceTexto].length) {
+        const timer = setTimeout(() => {
+          setCaracterActual(caracterActual + 1);
+          setTextoMostrado(textos[indiceTexto].substring(0, caracterActual + 1));
+        }, 70);
+        return () => clearTimeout(timer);
+      }
+    }
+  }, [caracterActual, indiceTexto, textos]);
+
+  const handleClick = () => {
+    if (indiceTexto < textos.length - 1) {
+      setIndiceTexto(indiceTexto + 1);
+      setCaracterActual(0);
+      setTextoMostrado('');
+      audioRef.current.play();
+    } else {
+      window.location.href = '/HomePage';
+    }
+  };
+
   return (
-    <> 
-    <section className='hero'>
-        <div className="imagenhero">
-        
-            
-        </div>
-    </section>
-    </>
-  )
-}
+    <div className='paginaoak'>
+      <audio src={sound2} controls loop autoPlay style={{ display: 'none' }} />
+      <div className="profesor">
+        <img src={profesor} alt="" />
+      </div>
+      <div className="cuadro-texto">
+      {textoMostrado} 
+      <button className='botonoak' onClick={handleClick}>
+        <img src={silly} alt="" />
+      </button>
+      </div>
+      <audio ref={audioRef} src={sound} />
+    </div>
+  );
+};
+
+export default Heropage;
